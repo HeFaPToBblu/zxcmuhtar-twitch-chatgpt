cb357a84ea580857be0d5fffd6a18bef23597004
HeFaPToBblu <95655467+HeFaPToBblu@users.noreply.github.com>
2023-04-08 21:19:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const readFile = promisify(fs.readFile)
 
 app.use(express.json({extended: true, limit: '1mb'}))
 
+
 app.all('/', (req, res) => {
 console.log("Just got a request!")
 res.send('Yo!')
 })
+
+let file_context = ""
+fs.readFile("./file_context.txt", 'utf8', function(err, data) {
+if (err) throw err;
+console.log(file_context);
+file_context = data;
+});
 
 app.get('/gpt/:text', async (req, res) => {
 const text = req.params.text
@@ -22,12 +30,12 @@ apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-const prompt ="\nQ:" + text + "\nA:";
+const prompt = file_context + "\n\nQ:" + text + "\nA:";
 console.log(prompt);
 
-const response = await openai.createChatCompletion({
-model: "gpt-3.5-turbo",
-messages: [{ role: "user", content: prompt}],
+const response = await openai.createCompletion({
+model: "text-davinci-003",
+prompt: prompt,
 temperature: 0.5,
 max_tokens: 300,
 top_p: 1,
@@ -35,7 +43,7 @@ frequency_penalty: 0,
 presence_penalty: 0,
 });
 if (response.data.choices) {
-res.send(response.data.choices[0].message.content)
+res.send(response.data.choices[0].text)
 } else {
 res.send("Something went wrong. Try again later!")
 }

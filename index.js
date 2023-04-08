c563c3f59026af2d7f5e39719974838116524e5b
HeFaPToBblu <95655467+HeFaPToBblu@users.noreply.github.com>
2023-04-08 20:23:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,9 +29,12 @@ app.get('/gpt/:text', async (req, res) => {
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{role: "system", content: "You are a chatbot on the twitch platform, so you should follow the twitch rules.You should have short answers in Russian.Be polite and funny to everyone."},
-          { role: "user", content: text}],
+          { role: "user", content: prompt}],
             temperature: 0.5,
             max_tokens: 300,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
     });
     if (response.data.choices) {
         res.send(response.data.choices[0].message.content)

33b8e1111812792e9988c501da96713ff973cd5b
HeFaPToBblu <95655467+HeFaPToBblu@users.noreply.github.com>
2023-04-09 22:59:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const response = await openai.createChatCompletion({
 model: "gpt-3.5-turbo",
 messages: [{role: "system", content: "You are a dog on the twitch platform, so you should follow the twitch rules.You must answer quickly and briefly in Russian.Be polite and funny to everyone.You have to talk like a human, like you have an opinion.Your image is a dog named Mukhtar.You are not a streamer.Your owner is a man with the nickname He_FaPToBblu.You must love Vika, she is your girlfriend, she has a nickname on twitch tsuuki_mrr."},
 { role: "user", content: prompt}],
-temperature: 0.5,
+temperature: 0.4,
 max_tokens: 300,
 top_p: 1,
 frequency_penalty: 0,

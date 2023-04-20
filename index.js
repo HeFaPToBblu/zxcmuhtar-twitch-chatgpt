7957be220b3f85eb0d06ffebec41eada9628aa7a
HeFaPToBblu <95655467+HeFaPToBblu@users.noreply.github.com>
2023-04-20 14:28:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const configuration = new Configuration({
 apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-
-const prompt ="\nQ:" + text + "\nA:";
+let text1 = text.replace(/\?/g, ',');
+const prompt ="\nQ:" + text1 + "\nA:";
 console.log(prompt);
 
 const response = await openai.createChatCompletion({
